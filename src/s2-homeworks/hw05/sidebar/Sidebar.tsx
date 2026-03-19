778a61a3d4748afa03d8a63680e678a1ce6f4ebf
stanislau <huzianokstanislau@gmail.com>
2026-03-19 19:22:11 +0100

    HW05 done

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -40,6 +40,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         to={PATH.JUNIOR}
                         onClick={handleClose}
                         // className={open ? s.active : ""}
+
                     >
                         Junior
                     </NavLink>
