c379611457c9b99fa039062e3d07fdafa36e5343
stanislau <huzianokstanislau@gmail.com>
2026-05-04 20:10:17 +0200

    HW11 done

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -13,10 +13,18 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 function HW11() {
     // for autotests // не менять // можно подсунуть в локалСторэдж нужные числа, чтоб увидеть как они отображаются
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
-    const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
+    const [value2, setValue2] = useState(restoreState<number[]>('hw11-value2', [0, 100]))
 
     const change = (event: any, value: any) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+if(Array.isArray(value)) {
+    setValue1(value[0])
+    setValue2(value)
+} else {
+    setValue2(value)
+}
+        console.log(event.target.value)
+        console.log(value)
     }
 
     return (
@@ -30,6 +38,9 @@ function HW11() {
                         <SuperRange
                             id={'hw11-single-slider'}
                             // сделать так чтоб value1 изменялось // пишет студент
+                            value={value1}
+                            onChange={change}
+                            name={'hw11-value1'}
 
                         />
                     </div>
@@ -38,9 +49,11 @@ function HW11() {
                         <SuperRange
                             id={'hw11-double-slider'}
                             // сделать так чтоб value1/2 изменялось // пишет студент
-
+                            value={value2}
+                            onChange={change}
+                            name={'hw11-value2'}
                         />
-                        <span id={'hw11-value-2'} className={s.number}>{value2}</span>
+                        <span id={'hw11-value-2'} className={s.number}>{value2[1]}</span>
                     </div>
                 </div>
             </div>
