db3f199b4985ab643322a8d50db18bb147709012
stanislau <huzianokstanislau@gmail.com>
2026-05-04 20:19:35 +0200

    HW11 fixed

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
-import { restoreState } from '../hw06/localStorage/localStorage'
+import {restoreState} from '../hw06/localStorage/localStorage'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 
 /*
@@ -17,14 +17,13 @@ function HW11() {
 
     const change = (event: any, value: any) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
-if(Array.isArray(value)) {
-    setValue1(value[0])
-    setValue2(value)
-} else {
-    setValue2(value)
-}
-        console.log(event.target.value)
-        console.log(value)
+        if (Array.isArray(value)) {
+            setValue1(value[0])
+            setValue2(value)
+        } else {
+            setValue1(value)
+            setValue2([value, value2[1]])
+        }
     }
 
     return (
